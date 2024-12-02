7f9c473d38da0707b1d3987c3d0396ef32cbc840
MGS-sails <info@ubtconsults.se>
2024-12-02 05:58:15 +0000

    Attempt at deploying new lambda function

--- demos/browser/server.js
+++ demos/browser/server.js
@@ -74,127 +74,6 @@ function serve(host = '127.0.0.1:8080') {
         const debugResponse = await debug.debug(request);
         respond(response, debugResponse.status, 'application/json', JSON.stringify(debugResponse.response, null, 2));
       }
-      // else if (request.method === 'POST' && requestUrl.pathname === '/meetings') {
-      //   console.log('REQ URL ', request);
-      //   // if (!requestUrl.query.region) {
-      //   //   respond(response, 400, 'application/json', JSON.stringify({ error: 'Need region parameter set if meeting has not yet been created' }));
-      //   // }
-      //   const meetingIdFormat = /^[a-fA-F0-9]{8}(?:-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12}$/
-      //   // let meeting = meetingTable[requestUrl.query.title];
-      //   let meeting = null;
-      //   try {
-      //     meeting = await chimeSDKMeetings.getMeeting({
-      //       MeetingId: requestUrl.query.title
-      //       // MeetingId: 'MGS_TEST'
-      //     });
-      //     // if (meetingIdFormat.test(requestUrl.query.title)) {
-      //     // if (meetingIdFormat.test("MGS_TEST")) {
-      //     //   meeting = await chimeSDKMeetings.getMeeting({
-      //     //     MeetingId: requestUrl.query.title
-      //     //   });
-      //     // }
-      //   } catch (error) {
-      //     console.info("Meeting ID doesn't exist as a conference ID: " + error);
-      //   }
-      //
-      //   if (!meeting) {
-      //     let request = {
-      //       ClientRequestToken: uuidv4(),
-      //       MediaRegion: 'us-east-1',
-      //       ExternalMeetingId: requestUrl.query.title.substring(0, 64),
-      //       // ExternalMeetingId: 'MGS_TEST',
-      //     };
-      //
-      //
-      //     request.MeetingFeatures = {};
-      //     request.MeetingFeatures.Audio = {
-      //       EchoReduction: 'AVAILABLE',
-      //     };
-      //     request.MeetingFeatures.Video = {
-      //       // MaxResolution: requestUrl.query.v_rs,
-      //       MaxResolution: 'HD',
-      //     };
-      //     request.MeetingFeatures.Content = {
-      //       // MaxResolution: requestUrl.query.c_rs,
-      //       MaxResolution: 'FHD',
-      //     };
-      //     request.MeetingFeatures.Attendee = {
-      //       MaxCount: 250,
-      //     };
-      //
-      //     // if (
-      //     //   requestUrl.query.ns_es === 'true' ||
-      //     //   requestUrl.query.v_rs === 'FHD' ||
-      //     //   requestUrl.query.v_rs === 'None' ||
-      //     //   requestUrl.query.c_rs === 'UHD' ||
-      //     //   requestUrl.query.c_rs === 'None' ||
-      //     //   requestUrl.query.a_cnt > 1 && requestUrl.query.a_cnt <= 250
-      //     // ) {
-      //     //   request.MeetingFeatures = {};
-      //     //   if (requestUrl.query.ns_es === 'true') {
-      //     //     request.MeetingFeatures.Audio = {
-      //     //       EchoReduction: 'AVAILABLE',
-      //     //     };
-      //     //   }
-      //     //   if (requestUrl.query.v_rs === 'FHD' || requestUrl.query.v_rs === 'None') {
-      //     //     request.MeetingFeatures.Video = {
-      //     //       MaxResolution: requestUrl.query.v_rs,
-      //     //     };
-      //     //   }
-      //     //   if (requestUrl.query.c_rs === 'UHD' || requestUrl.query.c_rs === 'None') {
-      //     //     request.MeetingFeatures.Content = {
-      //     //       MaxResolution: requestUrl.query.c_rs,
-      //     //     };
-      //     //   }
-      //     //   if (requestUrl.query.a_cnt > 1 && requestUrl.query.a_cnt <= 250) {
-      //     //     request.MeetingFeatures.Attendee = {
-      //     //       MaxCount: Number(requestUrl.query.a_cnt),
-      //     //     };
-      //     //   }
-      //     // }
-      //
-      //     console.info('Creating new meeting: ' + JSON.stringify(request));
-      //     meeting = await chimeSDKMeetings.createMeeting(request);
-      //     console.info('Created new meeting: ' + JSON.stringify(meeting));
-      //
-      //   }
-      //
-      //   meetingTable[requestUrl.query.title] = meeting;
-      //
-      //   await ddb.putItem({
-      //     TableName: 'recorder-demo-stack-Meetings-E07BQC85E26Y',
-      //     Item: {
-      //       'Title': { S: requestUrl.query.title },
-      //       // 'Title': { S: 'MGS_TEST' },
-      //       'Data': { S: JSON.stringify(meeting) },
-      //       'Attendees': { L: [
-      //         {
-      //           M: {
-      //             'Name': { S: 'MGS' },
-      //             'Username': { S: 'MGS_007' },
-      //             'MeetingPasscode': { S: '1234' }
-      //           },
-      //         },
-      //         {
-      //           M: {
-      //             'Name': { S: 'Yusuf' },
-      //             'Username': { S: 'YGS_007' },
-      //             'MeetingPasscode': { S: '4321' }
-      //           },
-      //         },
-      //         ] },
-      //       'TTL': {
-      //         N: `${Math.floor(Date.now() / 1000) + 60 * 60 * 24}` // clean up meeting record one day from now
-      //       }
-      //     }
-      //   });
-      //
-      //
-      //   const meetings = Object.keys(meetingTable).map((title) => {
-      //     return meetingTable[title].Meeting;
-      //   });
-      //   respond(response, 200, 'application/json', JSON.stringify(meetings, null, 2));
-      // }
 
       else if (request.method === 'POST' && requestUrl.pathname === '/meetings') {
         // Parse POST body data
@@ -207,9 +86,6 @@ function serve(host = '127.0.0.1:8080') {
         request.on('end', async () => {
           try {
             const postData = JSON.parse(body);
-            console.log('postData:', postData);
-            console.log('postData.attendees:', postData.attendees);
-            console.log('Is attendees array?', Array.isArray(postData.attendees));
             // Now you can access the data from the request body
             const meetingTitle = postData.title; // instead of requestUrl.query.title
 
@@ -294,10 +170,6 @@ function serve(host = '127.0.0.1:8080') {
         if (!requestUrl.query.title) {
           respond(response, 400, 'application/json', JSON.stringify({ error: 'Need parameters: title and name' }));
         }
-        /* MGS ATTEMPT TO GET MEETING FROM DYNAMO DB
-
-
-         */
         try {
           const result = await ddb.getItem({
             TableName: 'recorder-demo-stack-Meetings-E07BQC85E26Y',
@@ -307,7 +179,6 @@ function serve(host = '127.0.0.1:8080') {
               },
             },
           });
-          console.log('RESULT ', result);
           // Get the attendees array
           const attendees = result.Item.Attendees.L;
 
